d0b23d0aa1c533e9f174d4d5d89fe4c2789e1aaf
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-04-19 19:15:38 +0200

    created

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+let clicked = true;
+document.querySelector(".menu-bars").addEventListener("click", (e) => {
+  if (clicked) {
+      e.target.innerHTML = "×";
+      document.querySelector("nav").style.height = "60vh"
+      document.querySelector("nav").style.width = "100%"
+    clicked = false;
+  } else {
+    e.target.innerHTML = "≡";
+      document.querySelector("nav").style.height = "0"
+      document.querySelector("nav").style.width = "0"
+      clicked = true;
+  }
+});
